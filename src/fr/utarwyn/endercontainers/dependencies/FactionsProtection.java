9239eda24330b525d1de613b46be8c95d84940d0
Utarwyn <maxime.malgorn@laposte.net>
2016-07-10 17:14:58 +0200

    Version 1.0.6b - Fix "FactionsProtection" for new versions of Factions plugin

--- src/fr/utarwyn/endercontainers/dependencies/FactionsProtection.java
+++ src/fr/utarwyn/endercontainers/dependencies/FactionsProtection.java
@@ -5,6 +5,7 @@ import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.MPlayer;
 import com.massivecraft.massivecore.ps.PS;
 import fr.utarwyn.endercontainers.EnderContainers;
+import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -12,24 +13,23 @@ public class FactionsProtection {
 
     public static boolean canOpenEnderChestInFaction(Block b, Player p) {
         if (!EnderContainers.getInstance().getDependenciesManager().isDependencyLoaded("Factions")) return true;
-        boolean r = false;
         MPlayer mplayer = MPlayer.get(p);
 
-        if (mplayer == null) return r;
+        if (mplayer == null) return false;
 
         Faction playerFac = mplayer.getFaction();
         Faction currentFac = BoardColl.get().getFactionAt(PS.valueOf(b.getLocation()));
 
-        if (mplayer.isOverriding()) r = true;
+        if (mplayer.isOverriding()) return true;
 
-        if ((!r) && (currentFac != null)) {
-            if ((playerFac != null) && (currentFac.getName().equalsIgnoreCase(playerFac.getName()))) r = true;
-            if (currentFac.getName().equalsIgnoreCase("§2Wilderness") ||
-                    currentFac.getName().equalsIgnoreCase("§4WarZone") ||
-                    currentFac.getName().equalsIgnoreCase("§6SafeZone")) r = true;
+        if(currentFac != null) {
+            String facName = ChatColor.stripColor(currentFac.getName());
+
+            if (facName.equals("Wilderness") || facName.equals("WarZone") || facName.equals("SafeZone")) return true;
+            if ((playerFac != null) && (currentFac.getName().equals(playerFac.getName()))) return true;
         }
 
-        return r;
+        return false;
     }
 
 }
