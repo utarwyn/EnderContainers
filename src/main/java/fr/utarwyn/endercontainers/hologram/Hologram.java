3de717efa4c38977a6e0fc21e2a7f0d3341fb0ba
utarwyn <maxime.malgorn@laposte.net>
2019-12-30 17:35:12 +0100

    Fix holograms for 1.15 versions

--- src/main/java/fr/utarwyn/endercontainers/hologram/Hologram.java
+++ src/main/java/fr/utarwyn/endercontainers/hologram/Hologram.java
@@ -1,20 +1,15 @@
 package fr.utarwyn.endercontainers.hologram;
 
 import fr.utarwyn.endercontainers.EnderContainers;
-import fr.utarwyn.endercontainers.compatibility.ServerVersion;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.logging.Level;
 
 /**
  * This class is used to display a text above an enderchest block
  * if the option blockNametag was set to true.
- * This class uses packets and it's compatible from 1.8 to 1.14.
+ * This class uses packets and supports versions from 1.8 to 1.15.
  *
  * @author Utarwyn
  * @since 2.0.0
@@ -26,97 +21,6 @@ class Hologram {
      * in an hologram.
      */
     private static final double ABS = 0.23D;
-
-    /**
-     * Static field used to get all classes of the internal Minecraft server
-     */
-    private static final String NMS_PACKAGE = "net.minecraft.server.";
-
-    /**
-     * Reflection NMS Entity class
-     */
-    private static Class<?> nmsEntity;
-
-    /**
-     * Reflection NMS Packet class
-     */
-    private static Class<?> nmsPacket;
-
-    /**
-     * Reflection CraftWorld class
-     */
-    private static Class<?> craftWorld;
-
-    /**
-     * Reflection Packet class
-     */
-    private static Class<?> packetClass;
-
-    /**
-     * Reflection EntityLiving class
-     */
-    private static Class<?> entityLivingClass;
-
-    /**
-     * CraftChatMessages class for 1.13 holograms
-     */
-    private static Class<?> chatMessageClass;
-
-    /**
-     * IChatBaseComponent class for 1.13 holograms
-     */
-    private static Class<?> chatBaseComponentClass;
-
-    /**
-     * EntityTypes class for 1.14 holograms
-     */
-    private static Class<?> entityTypesClass;
-
-    /**
-     * Constructor of the ArmorStand class
-     */
-    private static Constructor<?> armorStandConstructor;
-
-    /**
-     * Constructor for the destroy packet
-     */
-    private static Constructor<?> destroyPacketConstructor;
-
-    static {
-        String version = ServerVersion.getBukkitVersion();
-
-        try {
-            Class<?> worldClass = Class.forName(NMS_PACKAGE + version + ".World");
-            Class<?> armorStandClass = Class.forName(NMS_PACKAGE + version + ".EntityArmorStand");
-            Class<?> destroyPacketClass = Class.forName(NMS_PACKAGE + version + ".PacketPlayOutEntityDestroy");
-
-            nmsEntity = Class.forName(NMS_PACKAGE + version + ".Entity");
-            craftWorld = Class.forName("org.bukkit.craftbukkit." + version + ".CraftWorld");
-            packetClass = Class.forName(NMS_PACKAGE + version + ".PacketPlayOutSpawnEntityLiving");
-            entityLivingClass = Class.forName(NMS_PACKAGE + version + ".EntityLiving");
-
-            // In 1.14+ versions, ArmorStand constructor has changed,
-            // we have to pass the type of entity to spawn in arguments.
-            if (ServerVersion.is(ServerVersion.V1_14)) {
-                entityTypesClass = Class.forName(NMS_PACKAGE + version + ".EntityTypes");
-                armorStandConstructor = armorStandClass.getConstructor(entityTypesClass, worldClass);
-            } else {
-                armorStandConstructor = armorStandClass.getConstructor(worldClass);
-            }
-
-            destroyPacketConstructor = destroyPacketClass.getConstructor(int[].class);
-
-            // Two classes used to format messages for holograms (needed in 1.13+ versions)
-            if (ServerVersion.isNewerThan(ServerVersion.V1_12)) {
-                chatMessageClass = Class.forName("org.bukkit.craftbukkit." + version + ".util.CraftChatMessage");
-                chatBaseComponentClass = Class.forName(NMS_PACKAGE + version + ".IChatBaseComponent");
-            }
-
-            nmsPacket = Class.forName(NMS_PACKAGE + version + ".Packet");
-        } catch (ReflectiveOperationException e) {
-            EnderContainers.getInstance().getLogger().log(Level.SEVERE, "Cannot initialize the hologram", e);
-        }
-    }
 
     /**
      * The title of the hologram (its content)
@@ -134,9 +38,9 @@ class Hologram {
     private Player player;
 
     /**
-     * The destroy packet object generated at the creation of the hologram
+     * Identifier of the spawned entity for the observer
      */
-    private Object destroyPacket;
+    private int entityId;
 
     /**
      * Construct an hologram and spawn it directly
@@ -149,6 +53,7 @@ class Hologram {
         this.player = player;
         this.title = title;
         this.location = location;
+        this.entityId = -1;
 
         this.spawn();
     }
@@ -163,14 +68,15 @@ class Hologram {
     }
 
     /**
-     * Destroy the hologram
-     * (Send the destroy packet to the player)
+     * Destroy the hologram for the observer with the stored entity id.
      */
     void destroy() {
-        try {
-            this.sendPacket(this.destroyPacket);
-        } catch (Exception e) {
-            EnderContainers.getInstance().getLogger().log(Level.SEVERE, "Cannot destroy the hologram", e);
+        if (this.entityId > -1) {
+            try {
+                NMSHologramUtil.destroyEntity(this.entityId, this.player);
+            } catch (ReflectiveOperationException e) {
+                EnderContainers.getInstance().getLogger().log(Level.SEVERE, "Cannot destroy the hologram", e);
+            }
         }
     }
 
@@ -181,100 +87,10 @@ class Hologram {
         Location displayLoc = this.location.clone().add(.5, ABS - 1.25D, .5);
 
         try {
-            Object packet = this.getPacket(
-                    displayLoc.getWorld(),
-                    displayLoc.getX(), displayLoc.getY(), displayLoc.getZ(),
-                    this.title
-            );
-
-            Field field = packetClass.getDeclaredField("a");
-            field.setAccessible(true);
-
-            this.destroyPacket = this.getDestroyPacket((int) field.get(packet));
-            this.sendPacket(packet);
+            this.entityId = NMSHologramUtil.spawnHologram(this.player, displayLoc, this.title);
         } catch (ReflectiveOperationException e) {
             EnderContainers.getInstance().getLogger().log(Level.SEVERE, "Cannot spawn the hologram", e);
         }
-    }
-
-    /**
-     * Returns the packet used to destroy the hologram
-     *
-     * @param id The id of the entity to destroy
-     * @return The destroy packet object
-     */
-    private Object getDestroyPacket(int... id) throws ReflectiveOperationException {
-        return destroyPacketConstructor.newInstance((Object) id);
-    }
-
-    /**
-     * Returns the packet used to spawn the hologram
-     *
-     * @param w    World
-     * @param x    X coordinate
-     * @param y    Y coordinate
-     * @param z    Z coordinate
-     * @param text Text to display
-     * @return The spawn packet object
-     */
-    private Object getPacket(World w, double x, double y, double z, String text) throws ReflectiveOperationException {
-        Object craftWorldObj = craftWorld.cast(w);
-
-        Method getHandleMethod = craftWorldObj.getClass().getMethod("getHandle");
-        Object entityObject;
-
-        // In 1.14+ versions, we need to specify a type to create an instance of an ArmorStand.
-        if (ServerVersion.is(ServerVersion.V1_14)) {
-            Object armorStandType = entityTypesClass.getField("ARMOR_STAND").get(null);
-            entityObject = armorStandConstructor.newInstance(armorStandType, getHandleMethod.invoke(craftWorldObj));
-        } else {
-            entityObject = armorStandConstructor.newInstance(getHandleMethod.invoke(craftWorldObj));
-        }
-
-        if (ServerVersion.isNewerThan(ServerVersion.V1_12)) {
-            Method fromStringOrNullMethod = chatMessageClass.getMethod("fromStringOrNull", String.class);
-            Object chatComponent = fromStringOrNullMethod.invoke(null, text);
-
-            Method setCustomName = entityObject.getClass().getMethod("setCustomName", chatBaseComponentClass);
-            setCustomName.invoke(entityObject, chatComponent);
-        } else {
-            Method setCustomName = entityObject.getClass().getMethod("setCustomName", String.class);
-            setCustomName.invoke(entityObject, text);
-        }
-
-        Method setCustomNameVisible = nmsEntity.getMethod("setCustomNameVisible", boolean.class);
-        setCustomNameVisible.invoke(entityObject, true);
-
-        if (ServerVersion.isOlderThan(ServerVersion.V1_10)) { // 1.8 / 1.9
-            Method setGravity = entityObject.getClass().getMethod("setGravity", boolean.class);
-            setGravity.invoke(entityObject, false);
-        } else {                          // 1.10+
-            Method setNoGravity = entityObject.getClass().getMethod("setNoGravity", boolean.class);
-            setNoGravity.invoke(entityObject, true);
-        }
-
-        Method setLocation = entityObject.getClass().getMethod("setLocation", double.class, double.class, double.class, float.class, float.class);
-        setLocation.invoke(entityObject, x, y, z, 0.0F, 0.0F);
-
-        Method setInvisible = entityObject.getClass().getMethod("setInvisible", boolean.class);
-        setInvisible.invoke(entityObject, true);
-
-        Constructor<?> cw = packetClass.getConstructor(entityLivingClass);
-        return cw.newInstance(entityObject);
-    }
-
-    /**
-     * Send a packet to the player linked to the hologram
-     *
-     * @param packet The packet to send
-     */
-    private void sendPacket(Object packet) throws ReflectiveOperationException {
-        Method getHandle = this.player.getClass().getMethod("getHandle");
-        Object entityPlayer = getHandle.invoke(this.player);
-        Object pConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
-        Method sendMethod = pConnection.getClass().getMethod("sendPacket", nmsPacket);
-
-        sendMethod.invoke(pConnection, packet);
     }
 
 }
---
+++ src/main/java/fr/utarwyn/endercontainers/hologram/NMSHologramUtil.java
@@ -0,0 +1,250 @@
+package fr.utarwyn.endercontainers.hologram;
+
+import fr.utarwyn.endercontainers.EnderContainers;
+import fr.utarwyn.endercontainers.compatibility.ServerVersion;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+
+/**
+ * This class is used to perform reflection things
+ * on server net classes to spawn holograms for all versions.
+ *
+ * @author Utarwyn
+ * @since 2.2.0
+ */
+public class NMSHologramUtil {
+
+    /**
+     * Static field used to get all classes of the internal Minecraft server
+     */
+    private static String nmsPackage;
+
+    /**
+     * Reflection NMS Entity class
+     */
+    private static Class<?> entityClass;
+
+    /**
+     * Reflection NMS Packet class
+     */
+    private static Class<?> packetClass;
+
+    /**
+     * Reflection CraftWorld class
+     */
+    private static Class<?> craftWorldClass;
+
+    /**
+     * CraftChatMessages class for 1.13+ holograms
+     */
+    private static Class<?> chatMessageClass;
+
+    /**
+     * IChatBaseComponent class for 1.13+ holograms
+     */
+    private static Class<?> chatBaseComponentClass;
+
+    /**
+     * EntityTypes class for 1.14+ holograms
+     */
+    private static Class<?> entityTypesClass;
+
+    /**
+     * Constructor of the ArmorStand class
+     */
+    private static Constructor<?> armorStandConstructor;
+
+    /**
+     * Constructor for the spawn packet
+     */
+    private static Constructor<?> spawnPacketConstructor;
+
+    /**
+     * Metadata Packet constructor for 1.15+ holograms
+     */
+    private static Constructor<?> metadataPacketConstructor;
+
+    /**
+     * Constructor for the destroy packet
+     */
+    private static Constructor<?> destroyPacketConstructor;
+
+    static {
+        String version = ServerVersion.getBukkitVersion();
+        nmsPackage = "net.minecraft.server." + version;
+
+        try {
+            Class<?> worldClass = getNMSClass("World");
+            Class<?> armorStandClass = getNMSClass("EntityArmorStand");
+            Class<?> destroyPacketClass = getNMSClass("PacketPlayOutEntityDestroy");
+            Class<?> spawnPacketClass = getNMSClass("PacketPlayOutSpawnEntityLiving");
+
+            packetClass = getNMSClass("Packet");
+            entityClass = getNMSClass("Entity");
+            craftWorldClass = Class.forName("org.bukkit.craftbukkit." + version + ".CraftWorld");
+
+            spawnPacketConstructor = spawnPacketClass.getConstructor(getNMSClass("EntityLiving"));
+            destroyPacketConstructor = destroyPacketClass.getConstructor(int[].class);
+
+            // 1.13+ :: text to display needed to be converted to a IChatBaseComponent component
+            if (ServerVersion.isNewerThan(ServerVersion.V1_12)) {
+                chatMessageClass = Class.forName("org.bukkit.craftbukkit." + version + ".util.CraftChatMessage");
+                chatBaseComponentClass = getNMSClass("IChatBaseComponent");
+            }
+
+            // 1.14+ :: the living entity constructor must be called with the entity type
+            if (ServerVersion.isNewerThan(ServerVersion.V1_13)) {
+                entityTypesClass = getNMSClass("EntityTypes");
+                armorStandConstructor = armorStandClass.getConstructor(entityTypesClass, worldClass);
+            } else {
+                armorStandConstructor = armorStandClass.getConstructor(worldClass);
+            }
+
+            // 1.15+ :: entity metadatas have to be sent in separate packet
+            if (ServerVersion.isNewerThan(ServerVersion.V1_14)) {
+                Class<?> packetClass = getNMSClass("PacketPlayOutEntityMetadata");
+                Class<?> dataWatcherClass = getNMSClass("DataWatcher");
+                metadataPacketConstructor = packetClass.getConstructor(int.class, dataWatcherClass, boolean.class);
+            }
+        } catch (ReflectiveOperationException e) {
+            EnderContainers.getInstance().getLogger().log(Level.SEVERE, "Cannot initialize the hologram", e);
+        }
+    }
+
+    /**
+     * Utility class.
+     */
+    private NMSHologramUtil() {
+        // Not implemented
+    }
+
+    /**
+     * Spawn a custom hologram (text, location) for a player.
+     *
+     * @param player   observer of the hologram
+     * @param location location of the hologram
+     * @param text     text to display with the hologram
+     * @return Entity identifier of the spawned hologram
+     * @throws ReflectiveOperationException error with reflection
+     * @see <a href="https://wiki.vg/Protocol#Spawn_Mob">Protocol for 1.8 to 1.14</a>
+     * @see <a href="https://wiki.vg/Pre-release_protocol#Spawn_Mob">Protocol for 1.15+</a>
+     */
+    public static int spawnHologram(Player player, Location location, String text) throws ReflectiveOperationException {
+        // First, we need to generate the fake armorstand
+        Object entity = createHologramEntity(location.getWorld(), location.getX(), location.getY(), location.getZ(), text);
+        int entityId = (int) entityClass.getDeclaredMethod("getId").invoke(entity);
+
+        // Send the spawn packet for 1.8+
+        sendPacket(player, spawnPacketConstructor.newInstance(entity));
+
+        // Send the metadata packet for 1.15+
+        if (ServerVersion.isNewerThan(ServerVersion.V1_14)) {
+            Method getDataWatcher = entityClass.getDeclaredMethod("getDataWatcher");
+            Object metadataPacket = metadataPacketConstructor.newInstance(entityId, getDataWatcher.invoke(entity), false);
+            sendPacket(player, metadataPacket);
+        }
+
+        return entityId;
+    }
+
+    /**
+     * Destroy a specific entity for a player by its identifier.
+     *
+     * @param entityId identifier of the entity to destroy
+     * @param observer player concerned by the deletion
+     * @throws ReflectiveOperationException error with reflection
+     */
+    public static void destroyEntity(int entityId, Player observer) throws ReflectiveOperationException {
+        sendPacket(observer, destroyPacketConstructor.newInstance((Object) new int[]{entityId}));
+    }
+
+    /**
+     * Send a packet to the player linked to the hologram.
+     *
+     * @param player the player who has to receive the packet
+     * @param packet the packet to send
+     * @throws ReflectiveOperationException error with reflection
+     */
+    private static void sendPacket(Player player, Object packet) throws ReflectiveOperationException {
+        Method getHandle = player.getClass().getMethod("getHandle");
+        Object entityPlayer = getHandle.invoke(player);
+        Object pConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
+        Method sendMethod = pConnection.getClass().getMethod("sendPacket", packetClass);
+
+        sendMethod.invoke(pConnection, packet);
+    }
+
+    /**
+     * Create the entity used to display an hologram.
+     *
+     * @param w    World
+     * @param x    X coordinate
+     * @param y    Y coordinate
+     * @param z    Z coordinate
+     * @param text Text to display
+     * @return The spawn packet object
+     * @throws ReflectiveOperationException error with reflection
+     */
+    private static Object createHologramEntity(World w, double x, double y, double z, String text) throws ReflectiveOperationException {
+        Object craftWorldObj = craftWorldClass.cast(w);
+        Method getHandleMethod = craftWorldObj.getClass().getMethod("getHandle");
+
+        Object entityObject;
+
+        // In 1.14+ versions, we need to specify a type to create an instance of an ArmorStand.
+        if (ServerVersion.isNewerThan(ServerVersion.V1_13)) {
+            Object armorStandType = entityTypesClass.getField("ARMOR_STAND").get(null);
+            entityObject = armorStandConstructor.newInstance(armorStandType, getHandleMethod.invoke(craftWorldObj));
+        } else {
+            entityObject = armorStandConstructor.newInstance(getHandleMethod.invoke(craftWorldObj));
+        }
+
+        // In 1.13+ versions, the custom name must be a IChatBaseComponent component
+        if (ServerVersion.isNewerThan(ServerVersion.V1_12)) {
+            Method fromStringOrNullMethod = chatMessageClass.getMethod("fromStringOrNull", String.class);
+            Object chatComponent = fromStringOrNullMethod.invoke(null, text);
+
+            Method setCustomName = entityObject.getClass().getMethod("setCustomName", chatBaseComponentClass);
+            setCustomName.invoke(entityObject, chatComponent);
+        } else {
+            Method setCustomName = entityObject.getClass().getMethod("setCustomName", String.class);
+            setCustomName.invoke(entityObject, text);
+        }
+
+        Method setCustomNameVisible = entityClass.getMethod("setCustomNameVisible", boolean.class);
+        setCustomNameVisible.invoke(entityObject, true);
+
+        if (ServerVersion.isOlderThan(ServerVersion.V1_10)) {   // 1.8 / 1.9
+            Method setGravity = entityObject.getClass().getMethod("setGravity", boolean.class);
+            setGravity.invoke(entityObject, false);
+        } else {                                                // 1.10+
+            Method setNoGravity = entityObject.getClass().getMethod("setNoGravity", boolean.class);
+            setNoGravity.invoke(entityObject, true);
+        }
+
+        Method setLocation = entityObject.getClass().getMethod("setLocation", double.class, double.class, double.class, float.class, float.class);
+        setLocation.invoke(entityObject, x, y, z, 0.0F, 0.0F);
+
+        Method setInvisible = entityObject.getClass().getMethod("setInvisible", boolean.class);
+        setInvisible.invoke(entityObject, true);
+
+        return entityObject;
+    }
+
+    /**
+     * Get an internal net Minecraft Server class.
+     *
+     * @param className name of the class to get
+     * @return the internal class found by the name
+     * @throws ClassNotFoundException thrown if the class is not found
+     */
+    private static Class<?> getNMSClass(String className) throws ClassNotFoundException {
+        return Class.forName(nmsPackage + "." + className);
+    }
+
+}
